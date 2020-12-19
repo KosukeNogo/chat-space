accd240c2c3fffbc8d0a619b1758b7390206d02f
KosukeNogo <kousuke.nogo@gmail.com>
2020-12-20 01:47:28 +0900

    Receive JSON with done method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,39 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image){
+      let html = `<div class="Chat-main__message-list">
+        <div class="Chat-main__message-list__info">
+          <div class="Chat-main__message-list__info__member-name">
+            ${message.user_name}
+          </div>
+          <div class="Chat-main__message-list__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Chat-main__message-list__message">
+          ${message.content}
+          <img class="Message__image" src="${message.image}"
+        </div>
+      </div>`
+    return html;
+    } else {
+      let html = `<div class="Chat-main__message-list">
+        <div class="Chat-main__message-list__info">
+          <div class="Chat-main__message-list__info__member-name">
+            ${message.user_name}
+          </div>
+          <div class="Chat-main__message-list__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Chat-main__message-list__message">
+          ${message.content}
+          <img class="Message__image" src="${message.image}"
+        </div>
+      </div>`
+    return html;
+    }
+  }
   $('.Chat-main__message-bottom__form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +45,10 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      console.log(html);
     })
   });
 });
