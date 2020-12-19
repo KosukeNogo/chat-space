15166570b390a08a5054d81187fd36b3694fe9fe
KosukeNogo <kousuke.nogo@gmail.com>
2020-12-20 00:48:12 +0900

    Swich HTML and JSON

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,15 @@
 $(function(){
-  $(function(){
-    $('.Chat-main__message-bottom__form').on('submit', function(e){
-      //e.preventDefault()
-      console.log("hoge")
-      let formData = new FormData(this);
-      let url = $(this).attr('action');
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-    });
+  $('.Chat-main__message-bottom__form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
