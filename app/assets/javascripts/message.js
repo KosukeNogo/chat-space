709dc487ef11017fa3495c5c3720d60c671bef3e
KosukeNogo <kousuke.nogo@gmail.com>
2020-12-20 00:20:58 +0900

    Work messages#create for submit form event

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,17 @@
 $(function(){
   $(function(){
     $('.Chat-main__message-bottom__form').on('submit', function(e){
-      e.preventDefault()
+      //e.preventDefault()
       console.log("hoge")
+      let formData = new FormData(this);
+      let url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
     });
   });
 });
